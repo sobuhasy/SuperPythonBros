821abf12edf104889a5688cdb8993565758dc257
sobuhasy <sobu.hasy@hotmail.com>
2023-03-27 08:44:34 +0200

    Added background image

--- main.py
+++ main.py
@@ -15,8 +15,27 @@ PLAYER_VEL = 4
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
+
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Blue.png")
 
     run = True
     while run:
@@ -26,6 +45,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(window, background, bg_image)
 
     pygame.quit()
     quit()
