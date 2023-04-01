954205526b59eb561bbfd53a92ddb972df790a30
sobuhasy <sobu.hasy@hotmail.com>
2023-04-01 14:17:14 +0200

    Animated the player

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ class Player(pygame.sprite.Sprite):
     COLOR = (233, 246, 120)
     GRAVITY = 1
     SPRITES = load_sprite_sheets("MainCharacters", "PinkMan", 32, 32, True)
+    ANIMATON_DELAY = 3
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)        # 0, 0 is the top left corner of the screen
         self.x_vel = 0
@@ -75,9 +76,26 @@ class Player(pygame.sprite.Sprite):
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
+        self.update_sprite()
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count // self.ANIMATON_DELAY) % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+        self.update()
+
+    def update(self):
+        self.rect = self.sprite.get_rect(topleft = (self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.sprite)
 
     def draw(self, win):
-        self.sprite = self.SPRITES["idle_" + self.direction][0]
+        # self.sprite = self.SPRITES["idle_" + self.direction][0]
         win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
